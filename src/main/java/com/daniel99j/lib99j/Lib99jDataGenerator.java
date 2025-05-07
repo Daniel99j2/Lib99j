4da8d200daf5e94bc6af87504326aeebc38c2885
Daniel99j2 <daniel99j1235@proton.me>
2025-05-07 08:03:44 +0800

    Initial commit

---
+++ src/main/java/com/daniel99j/lib99j/Lib99jDataGenerator.java
@@ -0,0 +1,11 @@
+package com.daniel99j.lib99j;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Lib99jDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
